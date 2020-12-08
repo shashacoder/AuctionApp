da74216744277952bf073516bf1f4a987f350bbe
Vidit Solanki <viditsolanki2@gmail.com>
2020-12-08 14:47:21 +0530

    changes in testing.py

--- testing.py
+++ testing.py
@@ -10,25 +10,25 @@ class AuctionLogin(unittest.TestCase):
 
     def test_login_in_auction_app(self):
         driver = self.driver
-        driver.get("http://127.0.0.1:8000/accounts/login/?next=/")
+        driver.get("http://127.0.0.1:8000/accounts/login/")
         self.assertIn("Online Auctions", driver.title)
         username = driver.find_element_by_id("id_username")
-        username.send_keys("ranka123")
+        username.send_keys("")                                                  #add username
         password=driver.find_element_by_id("id_password")
-        password.send_keys("Aditya@123")
+        password.send_keys("")                                                  #add password
         clicking=driver.find_element_by_id("submitButton")
         clicking.click()
-        loginbutton=driver.find_element_by_id("createAuction")
-        loginbutton.click()
-        titlename = driver.find_element_by_name("title")
-        titlename.send_keys("")                                                  #add title here
-        description = driver.find_element_by_name("description")
-        description.send_keys("")                                                #add description here
-        driver.find_element_by_name("image").send_keys(os.getcwd()+"/")          #add image here
-        val = driver.find_element_by_name("min_value")
-        val.send_keys("")                                                        #add value here
-        clickCreate=driver.find_element_by_id("submitAuction")
+        clickCreate=driver.find_element_by_id("createAuction")
         clickCreate.click()
+        titlename = driver.find_element_by_name("title")
+        titlename.send_keys("")                                                 #add title here
+        description = driver.find_element_by_name("description")
+        description.send_keys("")                                               #add description here
+        driver.find_element_by_name("image").send_keys(os.getcwd()+"/")         #add image here
+        val = driver.find_element_by_name("min_value")
+        val.send_keys("")                                                       #add value here
+        clickSubmit=driver.find_element_by_id("submitAuction")
+        clickSubmit.click()
         clickLogout = driver.find_element_by_id("logout")
         clickLogout.click()
 
